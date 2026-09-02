7df2b6e8b9ac436b9d50e1af070603b35aa8a3dd
Ethan Luchs <ethanluchs@vt.edu>
2026-09-02 19:16:50 -0400

    testing rtlsdr

--- src/main.c
+++ src/main.c
@@ -1,1 +1,18 @@
-int main(void) { return 0; }
+#include <rtl-sdr.h>
+#include <stdio.h>
+
+int main(void) {
+  uint32_t count = rtlsdr_get_device_count();
+
+  if (count == 0) {
+    fprintf(stderr, "No devices found\n");
+    return 1;
+  }
+
+  for (uint32_t i = 0; i < count; i++) {
+    const char *name = rtlsdr_get_device_name(i);
+    printf("Device %u is a %s\n", i, name);
+  }
+
+  return 0;
+}
