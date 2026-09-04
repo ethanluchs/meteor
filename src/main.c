f1216c5e8eba7501526de49b3d733ec99d619f43
Ethan Luchs <ethanluchs@vt.edu>
2026-09-04 17:02:06 -0400

    more changes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <rtl-sdr.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
   uint32_t count = rtlsdr_get_device_count();
+  uint8_t buf[16384]; // buffer to read raw rtlsdr bytes into
+  int n_read;
+  rtlsdr_read_sync(dev, buf, sizeof(buf), &n_read);
 
   if (count == 0) {
     fprintf(stderr, "No devices found\n");
@@ -14,5 +18,16 @@ int main(void) {
     printf("Device %u is a %s\n", i, name);
   }
 
+  return 0;
+}
+
+void print_bytes(void *data, int len) {
+  //*data means that we are given the value immediately
+  // so we can just reference *data and print from that
+
+  for (int i = 0; i < len; i++) {
+
+    printf("%x", *arr[i]);
+  }
   return 0;
 }
